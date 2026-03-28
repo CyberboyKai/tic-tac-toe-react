c7e60a5284f98e35ccd6b879d26912ce3c406add
KAI LEE <kailee@KAIui-MacBookAir.local>
2026-03-28 16:52:29 -0400

    Initial commit with cool neon styles

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,28 @@
+import './index.css'
+
+function Square() {
+  return <button className="square">1</button>;
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
+
